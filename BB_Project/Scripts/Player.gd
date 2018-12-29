9fd5ca3516d7e2f4fb3a9d6382f58d98d338b2e7
Cirey Francis <thecursedmerchant@gmail.com>
2018-12-29 17:09:20 -0600

    Added acceleration and friction for smoother movement, changed the collider to a capsule for the player

--- BB_Project/Scripts/Player.gd
+++ BB_Project/Scripts/Player.gd
@@ -1,11 +1,12 @@
 extends KinematicBody2D
 
 #Statistics
-const MOVE_SPEED = 200
-const JUMP_HEIGHT = -300
+const MAX_SPEED = 150
+const JUMP_HEIGHT = -200
 const GRAVITY_MULTIPLIER = 2.5 
 const GRAVITY = 10
 const UP = Vector2(0,-1)
+const ACCELERATION = 50
 
 #Properties
 var motion = Vector2()
@@ -15,33 +16,41 @@ func _physics_process(delta):
 	#Gravity
 	motion.y += GRAVITY
 	
+	#Fricion
+	var friction = false
+	
 	#Movement
 	if Input.is_action_pressed("ui_right"):
 		$Sprite.flip_h = false
 		$Sprite.play("Run")
-		motion.x = MOVE_SPEED
+		motion.x = min(motion.x + ACCELERATION, MAX_SPEED)
 	elif Input.is_action_pressed("ui_left"):
 		$Sprite.flip_h = true
 		$Sprite.play("Run")
-		motion.x = -MOVE_SPEED
+		motion.x = max(motion.x - ACCELERATION, -MAX_SPEED)
 	else:
 		$Sprite.play("Idle")
-		motion.x = 0
-		
-		
-	#Jumping
+		friction = true
+	
+	#Jump and Apply Friction
 	if is_on_floor():
-		if Input.is_action_just_pressed("ui_up"):
+		if Input.is_action_pressed("ui_up"):
 			motion.y = JUMP_HEIGHT
+		if friction == true:
+				motion.x = lerp(motion.x, 0, 0.2)
 	else:
+		#Checks if character is falling 
 		if motion.y > 0:
 			$Sprite.play("Fall")
-			motion.y += GRAVITY * GRAVITY_MULTIPLIER * delta 
+			#motion.y += GRAVITY * GRAVITY_MULTIPLIER * delta 
 		else:
 			$Sprite.play("Jump")
+			if friction == true:
+				motion.x = lerp(motion.x, 0, 0.05)
 	
 	#Apply Motion changes
 	motion = move_and_slide(motion, UP)
+	
 #	
 	#Read documentation on GD functions
 	pass
